8c3a00a7a4b174eec5ffbbc66e8a26dae1ca28e6
AlyBlt <aly.bulut@gmail.com>
2025-09-24 21:18:09 +0300

    Proje Taslağı Oluşturuldu.

---
+++ OkulYonetimUygulamasi/OkulYonetimUygulamasi/DersNotu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OkulYonetimUygulamasi
+{
+    internal class DersNotu
+    {
+        public DersNotu(string dersAdi, int not)
+        {
+            this.DersAdi = dersAdi;
+            this.Not = not;
+        }
+
+        public string DersAdi;
+        public int Not;
+    }
+}
---
+++ OkulYonetimUygulamasi/OkulYonetimUygulamasi/Okul.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OkulYonetimUygulamasi
+{
+    internal class Okul
+    {
+        // Okula ait herhnagi bir bir bilginin değiştirilmesi gerektiğinde, ilgili işlemler bu sınıfta yapılmalı.
+
+        public List<Ogrenci> Ogrenciler = new List<Ogrenci>();
+
+
+        public void OgrenciEkle(int no, string ad, string soyad, DateTime dogumTarihi, CINSIYET cinsiyet, SUBE sube)
+        {
+            // Ogrenci oluşacak
+
+            Ogrenci o = new Ogrenci();
+
+            o.No = no;
+            o.Ad = ad;
+            //
+            //
+            //
+            //
+
+
+            this.Ogrenciler.Add(o);
+        }
+
+        public void NotEkle(int no, string ders, int not)
+        {
+            Ogrenci o = this.Ogrenciler.Where(x => x.No == no).FirstOrDefault();
+
+            if (o != null)
+            {
+                //DersNotu dn = new DersNotu(ders,not);
+
+                //o.Notlar.Add(dn);
+
+
+                o.Notlar.Add(  new DersNotu(ders, not)  );
+            }
+        }
+    }
+}
